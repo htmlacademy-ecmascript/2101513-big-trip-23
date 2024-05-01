f7cfa00028e31833250b69d1eded073a4e4d8ec7
artur.ibragimov <artur.ibragimov@travelline.ru>
2024-05-01 21:30:28 +0400

    Разделил константы и моковые данные

--- src/constants.js
+++ src/constants.js
@@ -69,62 +69,9 @@ const EVENT_TYPES = [
 
 const DESTINATION_POINTS = ['Amsterdam', 'Geneva'];
 
-const OFFERS_OPTIONS = [
-  {
-    value: 'luggage',
-    title: 'Add luggage',
-    price: 50,
-    isChecked: true
-  },
-  {
-    value: 'comfort',
-    title: 'Switch to comfort class',
-    price: 80,
-    isChecked: true
-  },
-  {
-    value: 'meal',
-    title: 'Add meal',
-    price: 15
-  },
-  {
-    value: 'seats',
-    title: 'Choose seats',
-    price: 5
-  },
-  {
-    value: 'train',
-    title: 'Travel by train',
-    price: 40
-  }
-];
-
-const DESTINATION_DESCRIPTION_PROPERTIES = {
-  description: 'Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it\'s renowned for its skiing.',
-  photos: [
-    {
-      src: 'img/photos/1.jpg'
-    },
-    {
-      src: 'img/photos/2.jpg'
-    },
-    {
-      src: 'img/photos/3.jpg'
-    },
-    {
-      src: 'img/photos/4.jpg'
-    },
-    {
-      src: 'img/photos/5.jpg'
-    }
-  ]
-};
-
 export {
   FILTER_TYPES,
   SORTING_TYPES,
   EVENT_TYPES,
-  DESTINATION_POINTS,
-  OFFERS_OPTIONS,
-  DESTINATION_DESCRIPTION_PROPERTIES
+  DESTINATION_POINTS
 };
