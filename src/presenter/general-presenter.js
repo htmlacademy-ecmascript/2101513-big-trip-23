be9aa305c5d37a8572fb0b12ece85800ec8ed8de
artur.ibragimov <artur.ibragimov@travelline.ru>
2024-05-01 19:29:20 +0400

    Добавляет presenter

---
+++ src/presenter/general-presenter.js
@@ -0,0 +1,57 @@
+import Filters from '../view/filters.js';
+import Sorting from '../view/sorting.js';
+import RoutePoint from '../view/route-point.js';
+import RoutesList from '../view/routes-list.js';
+import FormCreating from '../view/form-creating.js';
+
+import {render} from '../render.js';
+
+export default class GeneralPresenter {
+  routesListInstance = new RoutesList();
+
+  constructor() {
+    this.filtersContainer = document.querySelector('.trip-controls__filters');
+    this.contentContainer = document.querySelector('.trip-events');
+  }
+
+  renderFilters() {
+    if (this.filtersContainer) {
+      render(new Filters(), this.filtersContainer);
+    }
+  }
+
+  renderSorting() {
+    if (this.contentContainer) {
+      render(new Sorting(), this.contentContainer);
+    }
+  }
+
+  renderFormCreating() {
+    if (this.contentContainer) {
+      render(new FormCreating(), this.routesListInstance.getElement());
+    }
+  }
+
+  renderRoutes() {
+    if (this.contentContainer) {
+      for (let i = 0; i < 3; i++) {
+        render(new RoutePoint(), this.routesListInstance.getElement());
+      }
+    }
+  }
+
+  renderRoutesList() {
+    if (this.contentContainer) {
+      render(this.routesListInstance, this.contentContainer);
+
+      this.renderFormCreating();
+      this.renderRoutes();
+    }
+  }
+
+  init() {
+    this.renderFilters();
+    this.renderSorting();
+    this.renderRoutesList();
+  }
+}
