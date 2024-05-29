f34bf1fbc5ec932fa28b8cbe35d559cdf782d3ce
artur.ibragimov <artur.ibragimov@travelline.ru>
2024-05-29 23:18:42 +0400

    Добавляет новые вспомогательные функции / константы для сортировки

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,8 +1,22 @@
-const getWordWithCapitalLetter = (word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
+import {ErrorMessages} from '../constants';
+
+const capitalizeLetter = (word) => `${word.charAt(0).toUpperCase()}${word.slice(1)}`;
 
 const updateItems = (items, newItem) => items.map((oldItem) => oldItem.id === newItem.id ? newItem : oldItem);
 
+const getSearchableValue = (value, searchableIndex, separator = '-') => value.trim().split(separator)[searchableIndex];
+
+const sortItems = (items, cb) => {
+  if (!Array.isArray(items) || typeof cb !== 'function') {
+    throw new Error(ErrorMessages.INVALID_ARGUMENTS);
+  }
+
+  return [...items].sort(cb);
+};
+
 export {
-  getWordWithCapitalLetter,
-  updateItems
+  capitalizeLetter,
+  updateItems,
+  getSearchableValue,
+  sortItems
 };
